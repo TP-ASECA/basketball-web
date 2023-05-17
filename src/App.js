ae575ae939a18d7a5ea3196e85e90f255a527508
nachobattolla <ignacio.fbattolla@ing.austral.edu.ar>
2023-05-17 17:06:28 -0300

    toast

--- src/App.js
+++ src/App.js
@@ -4,10 +4,22 @@ import NavBar from "./Components/NavBar";
 import ResultsScreen from "./Screen/ResultsScreen.js";
 import AddMatchScreen from "./Screen/AddMatchScreen";
 import MatchScreen from "./Screen/MatchScreen";
+import {ToastContainer} from "react-toastify";
 
 function App() {
   return (
     <>
+        <ToastContainer
+            style={{top:'9vh', fontSize: "18px"}}
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            pauseOnHover
+        />
         <NavBar/>
         <div className="App">
             <Routes>
