efe26bc713179e7d08510c5e5e03d49206daf0c1
nachobattolla <ignacio.fbattolla@ing.austral.edu.ar>
2023-06-21 15:19:17 -0300

    toast

--- cypress/e2e/viewMatch.cy.js
+++ cypress/e2e/viewMatch.cy.js
@@ -2,16 +2,10 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/')
 
-    cy.get('#ff80818187e8e81c0187e8e877b5000b').click()
+    cy.get('#ff808181882b6e8001882b6eec2a0000').click()
 
-    const expectedUrl = 'http://localhost:3000/match/ff80818187e8e81c0187e8e877b5000b'; // URL esperada de la página redirigida
+    const expectedUrl = 'http://localhost:3000/match/ff808181882b6e8001882b6eec2a0000'; // URL esperada de la página redirigida
 
     cy.location('href').should('eq', expectedUrl);
-
-    cy.get("#arrowId").click()
-
-    const expectedUrlArrow = 'http://localhost:3000/'; // URL esperada de la página redirigida
-
-    cy.location('href').should('eq', expectedUrlArrow);
   })
 })
